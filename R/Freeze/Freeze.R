4636ab26b889bba9156f23c2b3d4ff5260752f9d
Jeehun Park <jeee1228@gmail.com>
2017-07-17 21:13:32 +0900

    Freeze analysis

--- R/Freeze/Freeze.R
+++ R/Freeze/Freeze.R
@@ -13,13 +13,11 @@ original_2 = read.csv("02_파주-수도미터.csv", stringsAsFactors = F)
 original_3 = read.csv("03_수도미터위치정보+매칭.csv", stringsAsFactors = F)
 original_4 = read.csv("04_파주-급수관로.csv", stringsAsFactors = F)
 
+head(original_4)
+head(original_2)
+head(original_5)
 ### 데이터 전처리
 original_4 = original_4[c(2:5)]
-head(original_1)
-head(original_2)
-head(original_3)
-head(original_4)
-
 
 names(original_2) <- c('houseCode','address', 'address2', 'supplyStatus','businessType','meterCode',
                        'meterNum','meterGauge','use','smallBlock','installDate','supplyDate','rupturePack')
@@ -33,50 +31,55 @@ original_6$length <- as.double(original_6$length)
 
 allData <- original_6
 
-write.csv(x = allData, file = "allData.csv")
+head(original_1)
+head(original_2)
+head(original_3)
+head(original_4)
 
 
 original_1$수용가번호 <- gsub("-", "", original_1$수용가번호)
-
-
-
-
 ruptureSorting <- original_1[original_1$중분류 == '동파',]
-
-head(ruptureSorting)
-
+ruptureSorting$수용가번호 <- as.numeric(ruptureSorting$수용가번호)
+str(ruptureSorting)
 
 a = as.data.frame(table(ruptureSorting$수용가번호))
 
 
-a$Var1 <- as.character(a$Var1)
-a$Var1 = gsub("-", "", a$Var1)
+a$Freq = 1
+colnames(a)=c("houseCode", "rupture")
+#a = a[2:length(a$houseCode), ]
+as.numeric(a$houseCode)
 head(a)
+str(a$houseCode)
 
-b = filter(a, Freq>4)
-c = b[2:length(b), "Var1"]
-c
-str_replace(c, "-", "")
-head(allData)
-allData[, "houseCode"==c]
+allData = join(allData, a, by = 'houseCode')
 
-filter(a, Freq>=3)
+allData$rupture[is.na(allData$rupture), ] = 0
 
-View(a)
-head(ruptureSorting)
 
-table(original_1$중분류)
-original_1[original_1$상태 == '반려',]$민원내용%>%
-  grep("동파", ., value=T)
+#write.csv(x = allData, file = "allData.csv")
+allData <- read.csv("allData.csv", stringsAsFactors = F)
 
+
+### 지도위에 그리기
+paju = get_map(location = c(lon = 126.83, lat = 37.85), zoom = 11, maptype = "terrain")
+ggmap(paju, extent = "device")+
+  geom_point(data = original_3, aes(x=long, y=lat), size = 0.01)
 
 
 
-head(original_5)
-paju = get_map(location = "paju, South Korea", zoom = 11, maptype = "roadmap")
 
-ggmap(paju)+geom_point(data = original_3, aes(x=long, y=lat), size = 0.01)
+str(allData)
+allData$pipeGauge = factor(allData$pipeGauge)
+allData$pipeType = factor(allData$pipeType)
+allData$businessType = factor(allData$businessType)
+allData$rupture = factor(allData$rupture)
+allData$rupturePack = factor(allData$rupturePack)
+allData$meterGauge = factor(allData$meterGauge)
 
+glm("rupture ~ pipeType + pipeGauge + meterGauge + rupturePack + businessType", data = allData)
+
+allData$rupture
 
 
 
